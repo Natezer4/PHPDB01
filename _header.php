402d6752b1ba747502fe1c2b6d2652d08c87c68e
Natan Coelho <natancoelho21@gmail.com>
2022-04-11 16:44:20 -0300

    Aula 05

--- _header.php
+++ _header.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <title>Vitugo</title>
+    <title>Vitugo - <?php echo $title ?></title>
 </head>
 
 <body>
@@ -25,13 +26,13 @@
 
         <nav>
 
-            <a href="/" title="Página inicial"><i class="fa-solid fa-house-chimney"></i><span>Início</span></a>
+            <a href="/" title="Página inicial"><i class="fa-solid fa-house-chimney fa-fw"></i><span>Início</span></a>
 
-            <a href="/contatos" title="Faça contato"><i class="fa-solid fa-comment-dots"></i><span>Contatos</span></a>
+            <a href="/contacts.php" title="Faça contato"><i class="fa-solid fa-comment-dots fa-fw"></i><span>Contatos</span></a>
 
-            <a href="/sobre" title="Sobre..."><i class="fa-solid fa-circle-info"></i><span>Sobre</span></a>
+            <a href="/about" title="Sobre..."><i class="fa-solid fa-circle-info fa-fw"></i><span>Sobre</span></a>
 
-            <a href="/usuario" title="Usuário"><i class="fa-solid fa-user"></i><span>Usuário</span></a>
+            <a href="/user" title="Usuário"><i class="fa-solid fa-user fa-fw"></i><span>Usuário</span></a>
 
         </nav>
 
