167276c4b92a9627fe4b7de46061d7d76be0769e
Natan Coelho <natancoelho21@gmail.com>
2022-04-13 16:36:50 -0300

    Aula 06

--- _config.php
+++ _config.php
@@ -1,17 +1,57 @@
 <?php
 
-// Essa deve ser SEMPRE a primeira linha de código do site.
+/**
+ * Gera conteúdo sempre em UTF-8.
+ * DEVE SER sempre a primeira linha de código.
+ */
 header('Content-Type: text/html; charset=utf-8');
 
 /**
- * Conexão com o banco de dados:
- *     As variáveis abaixo devem ser definidas conforme o provedor de hospedagem.
- *     Os dados abaixo são do XAMPP.
+ * Dados de conexão com o banco de dados.
  */
 $db = array(
-    "hostname" => "localhost",
-    "database" => "vitugo",
-    "username" => "root",
-    "password" => ""
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'vitugo'
 );
-$conn = new mysqli($db["hostname"], $db["username"], $db["password"], $db["database"]);
+
+/**
+ * Linha de conexão com o banco de dados.
+ */
+$conn = new mysqli($db['hostname'], $db['username'], $db['password'], $db['database']);
+
+/**
+ * SQL de teste do banco de dados.
+ */
+$sql = "SELECT * FROM articles WHERE art_status = 'on'";
+
+/**
+ * Executa a query, armazenando o resultado em '$res'
+ */
+$res = $conn->query($sql);
+
+/**
+ * Loop para obter cada um dos registros.
+ * O método 'fetch_assoc()' retorna cada registro dentro de um array.
+ */
+while ($article = $res->fetch_assoc()) {
+
+    /**
+     * Exibindo valor da array no HTML.
+     */
+    echo '<pre>';
+    print_r($article);
+    echo '</pre>';
+}
+
+/**
+ * SQL de teste com o banco de dados.
+ */
+$sql = "INSERT INTO contacts (name, email, subject, message) VALUES ('Joca', 'joca@joca', 'Contato do Joca', 'Mensagem do Joca')";
+
+/**
+ * Executa a query, armazenando o resultado em '$res'
+ */
+$conn->query($sql);
+
