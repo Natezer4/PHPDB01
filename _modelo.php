76180c7a0b4c539f70dbb421c35bd9372eb110a1
Natan Coelho <natancoelho21@gmail.com>
2022-04-18 16:41:27 -0300

    Aula 006
    .

--- _modelo.php
+++ _modelo.php
@@ -1,7 +1,15 @@
 <?php
 
 /**
- * Variável que define o título desta página
+ * Arquivo que faz a configuração incial da página.
+ * Por exemplo, conecta-se ao banco de dados.
+ */
+require($_SERVER['DOCUMENT_ROOT'] . '/_config.php');
+
+/**
+ * Variável que define o título desta página.
+ * Essa variável é usada no arquivo "_header.php".
+ * OBS: para a página inicial (index.php) usaremos o 'slogan' do site.
  *     Referências:
  *     → https://www.w3schools.com/php/php_variables.asp
  *     → https://www.php.net/manual/pt_BR/language.variables.basics.php
@@ -9,7 +17,9 @@
 $title = "Quem tem fome tem pressa...";
 
 /**
- * Inclui o cabeçalho da página 
+ * Inclui o cabeçalho da página.
+ * A superglobal "$_SERVER['DOCUMENT_ROOT']" retorna o caminho da raiz do site no Windows.
+ * Ex.: C:\xampp\htdocs 
  *     Referências:
  *     → https://www.w3schools.com/php/php_includes.asp
  *     → https://www.php.net/manual/pt_BR/function.include.php
@@ -22,15 +32,20 @@ require($_SERVER['DOCUMENT_ROOT'] . '/_header.php');
 <section>
 
     <h2>Título da página</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, iste aliquam aperiam voluptatem molestias nemo odit unde modi cupiditate exercitationem doloremque quaerat soluta rerum quidem dignissimos officiis sapiente, aut alias!</p>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio soluta voluptatum consequatur voluptatibus cupiditate temporibus qui, nostrum deserunt minus laudantium in officia rem dignissimos facilis modi culpa error aliquam? Quam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, iste aliquam aperiam voluptatem
+        molestias nemo odit unde modi cupiditate exercitationem doloremque quaerat soluta rerum quidem
+        dignissimos officiis sapiente, aut alias!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio soluta voluptatum consequatur
+        voluptatibus cupiditate temporibus qui, nostrum deserunt minus laudantium in officia rem dignissimos
+        facilis modi culpa error aliquam? Quam?</p>
 
 </section>
 
 <aside>
 
     <h3>Lateral</h3>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, aperiam corporis culpa consequatur iusto.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, aperiam corporis culpa consequatur
+        iusto.</p>
 
 </aside>
 
